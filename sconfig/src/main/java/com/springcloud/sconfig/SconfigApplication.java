d4a87dc62636ab98bf9c3c678ea271312d7dddeb
xiongyongjie <1144303569@qq.com>
2018-04-01 19:20:40 +0800

    bug 2 for2 修复1

--- sconfig/src/main/java/com/springcloud/sconfig/SconfigApplication.java
+++ sconfig/src/main/java/com/springcloud/sconfig/SconfigApplication.java
@@ -7,6 +7,11 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableConfigServer
 public class SconfigApplication {
+	/**
+	*bug2 for fork
+	*/
+	private String bug2fork1;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SconfigApplication.class, args);
