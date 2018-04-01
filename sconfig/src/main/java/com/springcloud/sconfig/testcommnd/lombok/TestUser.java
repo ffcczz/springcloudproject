7822b987cba611f9d7b82cb5193ac6294b37cef5
xiongyongjie <1144303569@qq.com>
2018-04-01 18:59:40 +0800

    bug3x修复

--- sconfig/src/main/java/com/springcloud/sconfig/testcommnd/lombok/TestUser.java
+++ sconfig/src/main/java/com/springcloud/sconfig/testcommnd/lombok/TestUser.java
@@ -1,9 +1,31 @@
 package com.springcloud.sconfig.testcommnd.lombok;
 
 public class TestUser {
+    /**
+    *myname
+    */
+    private static String giveName;
+
+
     public static void main(String[] args){
       User u = User.builder().build();
       u.setAge(17);
       System.out.println(u.getAge());
+        try {
+            String lastnmae = "aaalastname";
+            main("mm",lastnmae);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
+    public static void main(String mm, String firstName){
+        String ac = firstName +  giveName;
+        myGetName(ac,"mynameis ok");
+    }
+
+    private static void myGetName(String s, String ac) {
+        System.out.println(s+":"+ac);
+    }
+
 }
