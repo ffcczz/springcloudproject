b56a70996358298f9b0610cdac08d0806816ccb9
xiongyongjie <1144303569@qq.com>
2018-04-14 23:31:00 +0800

    提交合并分支

--- mybatis/src/main/java/com/mybatis/op/TestController.java
+++ mybatis/src/main/java/com/mybatis/op/TestController.java
@@ -9,10 +9,11 @@ public class TestController {
 
     @GetMapping("list")
     public void printTest() {
-        System.out.println("my2222name999sy777so999u499988uuu");
-        System.out.println("mxx1mm= ");
-        for (int i = 0; i < 100; i++) {
-            System.out.println(i);
+        System.out.println("my2222na   me999sy777so999u499988uuu");
+        System.out.println("mxx1mm=999 ");
+        System.out.println("xxxx");
+          for (int i = 0; i < 100; i++) {
+        System.out.println(i);
         }
     }
 }
